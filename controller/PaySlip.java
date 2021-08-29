d72d1d58ff4df3ed0e96b07adb1cf06128cc79ad
lbrav <l.braverman.l@live.co.uk>
2021-08-29 20:45:09 +0100

    Created config class for JWT Implementation

--- controller/PaySlip.java
+++ controller/PaySlip.java
@@ -4,18 +4,37 @@ import com.payslip.model.Authentication;
 import com.payslip.model.JwtToken;
 import com.payslip.util.JwtTokenUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class PaySlip {
 
-    JwtTokenUtil jwtTokenUtil;
+
+    private AuthenticationManager authenticationManager;
+
+
+    private JwtTokenUtil jwtTokenUtil;
 
 
     @PostMapping("/Auth")
-    public ResponseEntity<JwtToken> authenticate(@RequestBody Authentication authentication) {
+    public ResponseEntity<JwtToken> authenticate(@RequestBody Authentication authentication) throws Exception {
+
+      try {
+          authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authentication.getUsername(),
+                  authentication.getPassword()));
+
+      }catch (DisabledException e) {
+          throw new Exception("USER_DISABLED", e);
+      } catch (BadCredentialsException e) {
+          throw new Exception("INVALID_CREDENTIALS", e);
+      }
 
 
         final String token = jwtTokenUtil.generateToken(null);
